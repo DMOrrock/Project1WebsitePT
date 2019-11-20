2c6c9e385df3839cc848550a74e2d18eaeb65803
victoria-skylan <50216078+victoria-skylan@users.noreply.github.com>
2019-11-20 03:03:38 -0700

    ommit some changes to the exercise info page

---
+++ Project1WebsitePT/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project1WebsitePT.Models
+{
+    public class Exercise { 
+        public string Name { get; set; }
+        public string MuscleGroup { get; set; }
+        public int RepNumber { get; set; }
+        public int Time { get; set; }
+        public string graphic { get; set; }
+        public string SimilarStretch1 { get; set; }
+        public string SimilarStretch2 { get; set; }
+
+}
+}
